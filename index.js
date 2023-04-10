d56e9faed32723170424721d8810018f70e5677b
niuzhiwei3 <niuzhiwei3@jd.com>
2023-04-10 22:20:27 +0800

    chore: log message

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const chatLimiter = rateLimit({
   windowMs: 3 * 60 * 60 * 1000, // 3 hoour
   max: CHAT_LIMITER,
   keyGenerator: (request, response) => {
-    console.log(request.body.messages)
+    console.log(request.ip)
     return request.ip
   },
   message: {
