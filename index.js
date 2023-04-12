f31470705c2c2c376427ed94e7fe5fcb465bd24f
VincentNeo <76011299@qq.com>
2023-04-12 13:49:36 +0800

    Update index.js

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ app.post("/v1/chat/completions", chatLimiter, async (req, res) => {
     });
     openaiRes.data.pipe(res);
   } catch (error) {
-    res.status(500).send(error);
+    res.send(error);
   }
 });
 
